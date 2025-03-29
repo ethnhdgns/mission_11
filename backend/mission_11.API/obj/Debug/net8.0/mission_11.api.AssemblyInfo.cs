84e23333780a20bfb8ea239fdafee51d98fca91a
ethnhdgns <ethanhudgins@outlook.com>
2025-03-29 15:39:20 -0600

    Final touches

--- backend/mission_11.API/obj/Debug/net8.0/mission_11.api.AssemblyInfo.cs
+++ backend/mission_11.API/obj/Debug/net8.0/mission_11.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mission_11.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56156e1ff0d4554df3ab560d91929c23d5c2faf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97aeae41d12b5e86998a110cc6752f8a0e34eaa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("mission_11.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mission_11.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
