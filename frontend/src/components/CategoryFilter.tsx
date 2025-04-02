22d6e0b1e79fd751605bc0b7b32d7f9ec4e78d00
ethnhdgns <ethanhudgins@outlook.com>
2025-04-02 17:49:03 -0600

    idek

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -12,7 +12,7 @@ function CategoryFilter({ selectedCategories, setSelectedCategories }: CategoryF
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch('https://bookstore-hudgins-backend.azurewebsites.net/Book/GetCategories');
+        const response = await fetch("https://bookstore-hudgins-backend.azurewebsites.net/book/GetCategories")
         const data = await response.json();
         console.log('Fetched categories:', data);
         setCategories(data);
