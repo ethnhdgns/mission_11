e5fea6123d3d8e7991276090f168f81e1b1563fa
ethnhdgns <ethanhudgins@outlook.com>
2025-04-02 16:47:29 -0600

    finishing soon

--- backend/mission_11.API/Controllers/BookController.cs
+++ backend/mission_11.API/Controllers/BookController.cs
@@ -85,7 +85,51 @@ namespace mission_11.api.Controllers
 
             return Ok(book); // Return the book data if found
         }
+        
+        
+        [HttpPost("AddBook")]
+        public IActionResult AddBook([FromBody] Books newBook)
+        {
+            _bookContext.Books.Add(newBook);
+            _bookContext.SaveChanges();
+            return Ok(newBook);
+        }
 
+        [HttpPut("UpdateBook/{bookId}")]
+        public IActionResult UpdateBook(int bookId, [FromBody] Books updatedBook)
+        {
+            var existingBook = _bookContext.Books.Find(bookId);
+
+            existingBook.Title = updatedBook.Title;
+            existingBook.Author = updatedBook.Author;
+            existingBook.Publisher = updatedBook.Publisher;
+            existingBook.ISBN = updatedBook.ISBN;
+            existingBook.Category = updatedBook.Category;
+            existingBook.PageCount = updatedBook.PageCount;
+            existingBook.Price = updatedBook.Price;
+
+
+            _bookContext.Books.Update(existingBook);
+            _bookContext.SaveChanges();
+
+            return Ok(existingBook);
+        }
+
+        [HttpDelete("DeleteBook/{bookId}")]
+        public IActionResult DeleteBook(int bookId)
+        {
+            var book = _bookContext.Books.Find(bookId);
+
+            if (book == null)
+            {
+                return NotFound(new {message = "Book not found"});
+            }
+
+            _bookContext.Books.Remove(book);
+            _bookContext.SaveChanges();
+
+            return NoContent();
+        }
 
     }
 
