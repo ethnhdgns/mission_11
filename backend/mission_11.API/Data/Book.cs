66fbc8a5e8e043316cef0bc5e39d659af8f6174d
ethnhdgns <80478748+ethnhdgns@users.noreply.github.com>
2025-04-04 19:27:01 -0600

    Update Book.cs

--- backend/mission_11.API/Data/Book.cs
+++ backend/mission_11.API/Data/Book.cs
@@ -10,7 +10,8 @@ public class Books
     public string? Title { get; set; }
     public string? Author { get; set; }
     public string? Publisher { get; set; }
-    public int? ISBN { get; set; }
+    public string? ISBN { get; set; }
+    public string? Classification { get; set; }
     public string? Category { get; set; }
     public int? PageCount { get; set; }
     public double? Price { get; set; }
